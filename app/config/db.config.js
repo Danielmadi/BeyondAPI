9b53b590c6913b287d4dba4440e91daa132561bf
daniel <Daniel.m@Dsilicon.local>
2020-08-27 12:57:53 +0300

    make it better 1

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "postgres",
-  PASSWORD: "123",
-  DB: "testdb",
+  HOST: "ec2-18-215-99-63.compute-1.amazonaws.com",
+  USER: "dadmevakeepefk",
+  PASSWORD: "349de12b97a73f2b2fc36f7b4fbe574ebe0cbdd541657bd4d9ef75bc0f818edd",
+  DB: "d15c0i14b2otvj",
   dialect: "postgres",
   pool: {
     max: 5,
